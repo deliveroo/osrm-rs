9d52e314e3e7d4bdf4f2b52ab0e02748d4a7269c
Michael Killough <michael.killough@deliveroo.co.uk>
2019-01-16 13:45:39 +0000

    Make Error Sync.
    It's a `std::string`, and we only ever read it.

--- src/errors.rs
+++ src/errors.rs
@@ -11,6 +11,7 @@ impl_drop!(OsrmcError, osrmc_sys::osrmc_error_destruct);
 
 // This is just a thin wrapper around a std::string.
 unsafe impl Send for OsrmcError {}
+unsafe impl Sync for OsrmcError {}
 
 impl Display for OsrmcError {
     fn fmt(&self, f: &mut fmt::Formatter) -> StdResult<(), fmt::Error> {
